87f55931d3833d1cbf8643db32b9c3f7c6dfe712
Sowmiya04Balachandran <sowmiya2641@gmail.com>
2023-10-24 11:43:02 +0530

    delete

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authSlice from './auth-slice';
+import darkMode from './darkMode';
+import inboxSlice from './inbox-slice';
+import isVisibleReducer from './isVisible';
+ const store = configureStore({
+  reducer: {
+    auth:authSlice,
+    theme:darkMode,
+    inbox:inboxSlice,
+    isVisible:isVisibleReducer
+  },
+})
+ export default store;
+ 
